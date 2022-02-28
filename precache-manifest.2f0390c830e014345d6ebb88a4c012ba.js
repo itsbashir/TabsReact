903ba152d6c8fb18711807e3abc0464eaaaeb56b
Bashir Mohamed <bam200@hotmail.co.uk>
2022-02-28 16:49:16 +0000

    first commit

---
+++ precache-manifest.2f0390c830e014345d6ebb88a4c012ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40837f6072b989dfc42034f465420244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c9e2cf94ce765f6a75",
+    "url": "/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c9e2cf94ce765f6a75",
+    "url": "/static/js/main.be7f2cff.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
